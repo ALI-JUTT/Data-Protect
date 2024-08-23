7d089f174e5605df09b6a73402f735737b5426ad
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2024-08-23 22:14:36 +0300

    Add files via upload

---
+++ PROTECTOR.py
@@ -0,0 +1,10 @@
+import os, platform
+try:
+        import requests
+except:
+        os.system('pip2 install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == "64bit":
+        from Asad import menu
+        menu()
