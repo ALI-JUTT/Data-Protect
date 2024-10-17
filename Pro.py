683cb80cba4aa9f88452a15c9490dcb9635561f4
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2024-10-17 06:57:26 +0300

    Update Pro.py

--- Pro.py
+++ Pro.py
@@ -1,16 +1,15 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from pro import menu
-    menu()
-elif bit == '32bit':
-    from pro import menu
-    menu()
-else:
-    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
-    os.system('exit')
+#coding=utf-8
+import os
+os.system('xdg-open https://www.facebook.com/@okiew.you')
+os.system("clear")
+print('\033[0;97m [’¸] \033[92mChecking For Updates...')
+os.system('git pull --quiet 2>/dev/null')
+x="\033[0;97mScript is On Update" 
+y="Please Wait For Next Update"
+z="Authorized by : ASAD ALI"
+print(a) 
+print(l) 
+print(i)
+input(f"\n\033[1;92m Press Enter For join our WhatsApp Group ")
+os.system('xdg-open https://chat.whatsapp.com/ELnYuwpJEii7UhQ0xP7yu1')
+exit()
