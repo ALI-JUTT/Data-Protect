fce43dae32796c3a42a3614566545a1b8625f920
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2024-10-20 17:47:06 +0300

    Update Pro.py

--- Pro.py
+++ Pro.py
@@ -1,15 +1,23 @@
 #coding=utf-8
-import os
-os.system('xdg-open https://www.facebook.com/@okiew.you')
-os.system("clear")
-print('\033[0;97m [’Made Bey Asad¸] \033[92mChecking For Updates...')
-os.system('git pull --quiet 2>/dev/null')
-a="\033[0;97mScript is On Update" 
-l="Please Wait For Next Update"
-i="Authorized by : ASAD ALI"
-print(a) 
-print(l) 
-print(i)
-input(f"\n\033[1;92m Press Enter For join our WhatsApp Group ")
-os.system('xdg-open https://chat.whatsapp.com/ELnYuwpJEii7UhQ0xP7yu1')
-exit()
+import os, sys, platform
+os.system("git pull")
+os.system('rm -rf pro.so')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf pro.so')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('pro.so'):
+        os.system('curl -L https://github.com/ALI-JUTT/Pro/blob/main/pro.cpython-312.so?raw=true -o pro.so') 
+        import pro
+    else:
+        import pro
+ 
+elif bit == '32bit':
+    print("\033[1;90m [\033[1;91m Sorry Baby 32Bit Not Supported! ðŸ¥ºðŸ’”\033[1;90m]");exit()
+ 
